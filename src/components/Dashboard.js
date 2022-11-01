dba89c1d2fb6ab0e7bca2d717638263a6bf81f41
Akane <zarathustra47@yahoo.com>
2022-11-01 15:28:29 +0000

    Make corrections in response to feedback

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,20 +6,19 @@ const Dashboard = (props) => {
         fetch('https://pacific-citadel-03998.herokuapp.com/api/reviews')
         .then(res => res.json())
         .then((data) => {
-            console.log(data);
             setReviews(data.reviews);
         })
     }, []);
     return (
     <ul>
         {reviews.map((item) => (
-            <div class="review-box">
-                <img class="review-image" src={item.review_img_url} alt={item.title}/>
+            <li key={item.title} class="review-box">
+                <img className="review-image" src={item.review_img_url} alt={item.title}/>
                 <p>{item.title}</p>
                 <p class="designer">{item.designer}</p>
                 <p>Review by {item.owner}</p>
                 <p class="review-text">{item.review_body}</p>
-            </div>
+            </li>
         ))}
     </ul>
     );
